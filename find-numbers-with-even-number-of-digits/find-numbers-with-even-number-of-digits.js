1511748baa118699e046c8ab72b222d457b4a5f8
Akash Soni-2051 <104122208+akashsoni0786@users.noreply.github.com>
2024-01-09 11:48:18 +0530

    Time: 50 ms, Memory: 44.2 MB - LeetHub

---
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNumbers = function(nums) {
+    let count = 0;
+    for(let i=0;i<nums.length;i++){
+        let len = String(nums[i]).length;
+        if(len%2 == 0) count++
+    }
+    return count;
+};

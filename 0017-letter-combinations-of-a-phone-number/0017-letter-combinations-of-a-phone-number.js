dacbd59af1bf111c22de701f80fef5730f75bc27
Akash Soni-2051 <104122208+akashsoni0786@users.noreply.github.com>
2024-01-12 18:16:40 +0530

    Time: 48 ms (76.34%), Space: 42.2 MB (19.48%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+let digits_obj = {
+  0: ["0"],
+  1: ["1"],
+  2: ["a", "b", "c"],
+  3: ["d", "e", "f"],
+  4: ["g", "h", "i"],
+  5: ["j", "k", "l"],
+  6: ["m", "n", "o"],
+  7: ["p", "q", "r", "s"],
+  8: ["t", "u", "v"],
+  9: ["w", "x", "y", "z"],
+};
+var letterCombinations = function (digits) {
+    if(digits == "") return [];
+    let ans = [];
+    const combination = (idx,curr) => {
+        if(idx == digits.length) { ans.push(curr.join(''));
+    return;}
+        let letters = digits_obj[digits[idx]];
+        for(let ltr of letters) {
+            curr[idx] = ltr;
+            combination(idx+1,curr);
+        }
+    }
+    combination(0,[])
+    return ans;
+};

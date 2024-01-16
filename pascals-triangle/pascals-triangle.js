519ed6f5ca2880e4274e4b852bc0d6cc8bc1cea8
Akash Soni-2051 <104122208+akashsoni0786@users.noreply.github.com>
2024-01-16 22:55:44 +0530

    Time: 52 ms, Memory: 42.6 MB - LeetHub

---
+++ pascals-triangle/pascals-triangle.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+     if (numRows === 0) {
+        return [];
+    }
+
+    const result = [[1]];
+
+    for (let i = 1; i < numRows; i++) {
+        const prevRow = result[i - 1];
+        const newRow = [1];
+
+        for (let j = 1; j < prevRow.length; j++) {
+            newRow[j] = prevRow[j - 1] + prevRow[j];
+        }
+
+        newRow.push(1);
+        result.push(newRow);
+    }
+
+    return result;
+};

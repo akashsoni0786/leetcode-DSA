907577d6e4c0456d6ad15b136a15d2905eca485e
Akash Soni-2051 <104122208+akashsoni0786@users.noreply.github.com>
2024-02-01 22:29:02 +0530

    Time: 331 ms (60.63%), Space: 79.5 MB (95.87%) - LeetHub

---
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[][]}
+ */
+var divideArray = function(nums, k) {
+    nums.sort((a,b)=>a-b);
+    let ans = [];
+    for(let i=0;i<nums.length;i+=3){
+        if(nums[i+2]-nums[i] <= k){
+            ans.push([nums[i],nums[i+1],nums[i+2]])
+        }else return [];
+    }
+    return ans;
+};

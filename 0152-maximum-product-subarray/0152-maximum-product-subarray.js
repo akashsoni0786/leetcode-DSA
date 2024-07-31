39747407db464d25ddf34839f2e40cebd19d5b44
Akash Soni-2051 <akashsoni@cedcommerce.com>
2024-07-31 09:54:18 +0530

    Time: 60 ms (49.94%), Space: 51.6 MB (17.97%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.js
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -3,17 +3,20 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    if(nums.length == 0) return 0;
-    
-    let maxP = nums[0];
-    let minP = nums[0];
-    let res = nums[0];
-    
-    for(let i=1;i<nums.length;i++){
-        if(nums[i] < 0) [minP,maxP] = [maxP,minP];
-        minP = Math.min(nums[i], minP*nums[i])
-        maxP = Math.max(nums[i], maxP*nums[i])
-        res = Math.max(res,maxP)
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0];
+    let n = nums.length;
+    let max = nums[0];
+    let min = nums[0];
+    let result = nums[0];
+    for(let i=1; i<n; i++) {
+        if(nums[i] < 0)  [max,min] = [min,max];
+        
+        max = Math.max(nums[i] * max, nums[i]);
+        min = Math.min(nums[i] * min, nums[i]);
+        
+        result = Math.max(max, result)
+
     }
-    return res;
+    return result;
 };

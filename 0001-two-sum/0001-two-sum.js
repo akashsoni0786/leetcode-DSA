8ad3e26d5e3b446a49a92be09c2d668e94e2afe3
Akash Soni <aakashkumarsoni0786@gmail.com>
2025-04-14 14:19:46 +0530

    Time: 10 ms (45.68%), Space: 56.8 MB (10.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let ans = [];
-    let n = nums.length;
-    let obj  = {};
-    for(let i=0;i<n;i++) {
-        obj[nums[i]] = i;
-    }
-    for(let i=0;i<n;i++) {
-        let val = target - nums[i]
-        if(obj[val] && obj[val] != i) ans = [i,obj[val]].sort((a,b)=>a-b)
-    }
-    return ans;
+ let ans = [];
+ let obj = {};
+ for(let i=0;i<nums.length;i++){
+    obj[nums[i]] = i;
+ }
+ for(let i=0;i<nums.length;i++){
+    let val = target - nums[i];
+    if(obj[val] && obj[val] != i) ans=[i,obj[val]]
+ }
+ return ans.sort((a,b)=>a-b)
 };
